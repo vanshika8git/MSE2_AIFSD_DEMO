b0f597cf77fc7569eab89cba7fcd841214c2ad49
Vanshika Aggarwal <vanshikaagg30@gmail.com>
2026-04-23 16:21:14 +0530

    frontend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api"
+    baseURL: "https://aifsd-mse2-n5ma.onrender.com/api"
 });
 
 // Attach token automatically
